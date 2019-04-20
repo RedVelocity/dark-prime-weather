d66c926c9c44dea17e3c7b0b14997b2746062fa9
shahnkha <blooshaanster@gmail.com>
2019-04-20 16:48:08 +0530

    revert to mapbox geocoding, better/faster results

--- src/components/layout/WeatherSearch.js
+++ src/components/layout/WeatherSearch.js
@@ -15,13 +15,14 @@ export default class WeatherSearch extends Component {
     };
 
     componentDidMount() {
-      if(!this.props.isLoaded) {
+      const { isLoaded, toggleLoading, performSearch} = this.props;
+      if(!isLoaded) {
         if ("geolocation" in navigator) {
           /* geolocation is available */
           navigator.geolocation.getCurrentPosition((position) => {
             this.setState({ latitude:position.coords.latitude, longitude:position.coords.longitude });
-            this.props.toggleLoading();
-            this.props.performSearch(position.coords.latitude, position.coords.longitude);
+            toggleLoading();
+            performSearch(position.coords.latitude, position.coords.longitude);
           });
         } else {
           /* geolocation IS NOT available */
@@ -30,31 +31,41 @@ export default class WeatherSearch extends Component {
     }
 
     loadSuggestions = (e) => {
-      // const api = encodeURI(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.state.place}.json?access_token=${process.env.REACT_APP_MAPBOX_KEY}&types=place&proximity=${this.state.longitude},${this.state.latitude}&language=en`)
-      // axios.get(api)
-      //   .then(res => {
-      //     this.setState({suggestions: res.data.features.map((feature) => feature.place_name), features: res.data.features})
-      //     // console.log('place state', this.state);
-      //   }).catch((error) => {
-      //     console.log(error);
-      //   })
-      
-      const api = encodeURI(`https://api.opencagedata.com/geocode/v1/json?q=${this.state.place}&key=${process.env.REACT_APP_OPENCAGE_KEY}&proximity=${this.state.latitude}, ${this.state.longitude}&language=en-in&limit=5&min_confidence=1&no_annotations=1`)
+      const api = encodeURI(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.state.place}.json?access_token=${process.env.REACT_APP_MAPBOX_KEY}&types=place&proximity=${this.state.longitude},${this.state.latitude}&language=en`)
       axios.get(api)
         .then(res => {
-          this.setState({suggestions: res.data.results.map((feature) => feature.formatted), features: res.data.results})
-          // console.log('place state', res.data.results);
+          if(res.data.features.length !== 0) {
+          this.setState({suggestions: res.data.features.map((feature) => feature.place_name), features: res.data.features})
+          // console.log('place state', this.state);
+        } else this.setState({ ...this.state, features:null, suggestions: ['Place Not Found'] })
         }).catch((error) => {
           console.log(error);
         })
+      
+      // const api = encodeURI(`https://api.opencagedata.com/geocode/v1/json?q=${this.state.place}&key=${process.env.REACT_APP_OPENCAGE_KEY}&proximity=${this.state.latitude}, ${this.state.longitude}&language=en-in&limit=5&min_confidence=1&no_annotations=1&abbrv=1`)
+      // axios.get(api)
+      //   .then(res => {
+      //     if(res.data.results.length !== 0) {
+      //       this.setState(
+      //         {suggestions: res.data.results.map((feature) => feature.formatted), features: res.data.results})
+      //     // console.log('place state', res.data.results.length);
+      //     } else this.setState({ ...this.state, fetaures:null, suggestions: ['Place Not Found'] })
+      //   }).catch((error) => {
+      //     console.log(error);
+      //   })
     }
 
     onSelect = (e) => {
-      const feature = this.state.features.filter((feature) => feature.formatted === e.value);
-      this.setState({latitude:feature[0].geometry.lat, longitude:feature[0].geometry.lng});
-      // console.log('selected co-ords',feature[0].geometry.lat, latitude:feature[0].geometry.lat, this.state);
-      this.props.toggleLoading();
-      this.props.performSearch(feature[0].geometry.lat, feature[0].geometry.lng);
+      if(this.state.features !== null && this.state.features !== undefined) {
+        const { toggleLoading, performSearch} = this.props;
+        const feature = this.state.features.filter((feature) => feature.place_name === e.value);
+        if(feature.length !== 0) {
+          this.setState({latitude:feature[0].geometry.coordinates[1], longitude:feature[0].geometry.coordinates[1]});
+        // console.log('selected co-ords',feature[0].geometry.lat, latitude:feature[0].geometry.lat, this.state);
+          toggleLoading();
+          performSearch(feature[0].geometry.coordinates[1], feature[0].geometry.coordinates[1]);
+        } else this.setState({place: ''})
+      } else this.setState({place: ''})
     }
 
     render() {
