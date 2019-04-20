d4192272d7d7a30d55f490b887f7b10901cc4d4b
shahnkha <blooshaanster@gmail.com>
2019-04-21 01:43:18 +0530

    weather graph init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import 'primereact/resources/themes/nova-colored/theme.css';
 import 'primeicons/primeicons.css';
 import './App.css';
 import WeatherTabs from './components/layout/WeatherTabs';
+// import Moment from 'react-moment';
+import moment from 'moment';
+import {Chart} from 'primereact/chart';
 
 class App extends Component {
 
@@ -27,11 +30,11 @@ class App extends Component {
   }
 
   performSearch = (lat, lon) => {
-    const exclude = '[minutely,hourly,daily,flags]';
+    const exclude = '[minutely,hourly,flags]';
     const api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_DARKSKY_KEY}/${lat},${lon}?units=ca&exclude=${exclude}`;
     axios.get(api)
       .then(res => {
-        // console.log(res.data);
+        console.log('weather data', res.data);
         return this.setState({ weather: res.data, viewport:{...this.state.viewport, latitude: res.data.latitude, longitude: res.data.longitude, zoom:10}, isLoaded: true, isLoading: false })
       }).catch((error) => {
         console.log(error);
@@ -39,9 +42,36 @@ class App extends Component {
   }
 
   render() {
+      const { daily } = this.state.weather;
+      console.log('daily', daily);
+      let data = null;
+      if(this.state.isLoaded) {
+          data = {
+          labels: daily.data.map((daily) => moment.unix(daily.time).format('DD/MM')),
+          datasets: [
+              {
+                  label: 'Temperature High',
+                  data: daily.data.map((daily) => daily.apparentTemperatureHigh),
+                  fill: false,
+                  backgroundColor: '#42A5F5',
+                  borderColor: '#42A5F5'
+              }
+              // {
+              //     label: 'Second Dataset',
+              //     data: [28, 48, 40, 19, 86, 27, 90],
+              //     fill: false,
+              //     backgroundColor: '#66BB6A',
+              //     borderColor: '#66BB6A'
+              // }
+          ]   
+      };
+      console.log('data', data);
+    }
     return (
+      
       <div className="Container">
-          {/* {console.log(this.state)}          */}
+      {(data !== null) &&
+      <Chart type="line" data={data} /> }
           <WeatherTabs 
             weatherState={this.state} 
             performSearch={this.performSearch} 
