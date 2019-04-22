7aaeee4797393498b1eaab42552d8186ecc2c947
shahnkha <blooshaanster@gmail.com>
2019-04-22 12:33:11 +0530

    resize layout to vw/vh

--- src/components/layout/WeatherChart.js
+++ src/components/layout/WeatherChart.js
@@ -1,41 +1,42 @@
-import React from 'react'
-import moment from 'moment';
-import {Chart} from 'primereact/chart';
+import React from "react";
+import moment from "moment";
+import { Chart } from "primereact/chart";
 // import Media from 'react-media';
-import MediaQuery from 'react-responsive';
+import MediaQuery from "react-responsive";
 
-function WeatherChart( {weatherState} ) {
-
-    let daily = null;
-    let data = null;
-    // console.log('chart props', weather, isLoaded);
-    if(weatherState.isLoaded && !weatherState.isLoading) {
-        daily = weatherState.weather.daily;
-        // console.log('daily', daily);
-        data = {
-        labels: daily.data.map((daily) => moment.unix(daily.time).format('ddd DD/MM')),
-        datasets: [
-            {
-                label: 'Temperature High',
-                data: daily.data.map((daily) => daily.apparentTemperatureHigh),
-                fill: false,
-                backgroundColor: '#f7a800',
-                borderColor: '#f7a800'  
-            },
-            {
-                label: 'Temperature Low',
-                data: daily.data.map((daily) => daily.apparentTemperatureLow),
-                fill: false,
-                backgroundColor: '#42A5F5',
-                borderColor: '#42A5F5'
-            }
-        ]   
+function WeatherChart({ weatherState }) {
+  let daily = null;
+  let data = null;
+  // console.log('chart props', weather, isLoaded);
+  if (weatherState.isLoaded && !weatherState.isLoading) {
+    daily = weatherState.weather.daily;
+    // console.log('daily', daily);
+    data = {
+      labels: daily.data.map(daily =>
+        moment.unix(daily.time).format("ddd DD/MM")
+      ),
+      datasets: [
+        {
+          label: "Temperature High",
+          data: daily.data.map(daily => daily.apparentTemperatureHigh),
+          fill: false,
+          backgroundColor: "#f7a800",
+          borderColor: "#f7a800"
+        },
+        {
+          label: "Temperature Low",
+          data: daily.data.map(daily => daily.apparentTemperatureLow),
+          fill: false,
+          backgroundColor: "#42A5F5",
+          borderColor: "#42A5F5"
+        }
+      ]
     };
     // console.log('data', data);
   }
   return (
     <div>
-    {/* <Media query="(max-width: 500px)">
+      {/* <Media query="(max-width: 500px)">
           {matches =>
             matches ? (              
             <div>      
@@ -48,21 +49,29 @@ function WeatherChart( {weatherState} ) {
             )
           }
     </Media> */}
-    <MediaQuery maxWidth={500}>
-    {(matches) => {
-    if (matches) {
-      return <div>      
-      {(data !== null) && <Chart type="line" data={data} width="380px" /> }
-    </div>;
-    } else {
-      return <div>
-      {(data !== null) && <Chart type="line" data={data} width="500px" /> }
+      <MediaQuery maxWidth={500}>
+        {matches => {
+          if (matches) {
+            return (
+              <div>
+                {data !== null && (
+                  <Chart type="line" data={data} width="90vw" />
+                )}
+              </div>
+            );
+          } else {
+            return (
+              <div>
+                {data !== null && (
+                  <Chart type="line" data={data} width="38vw" />
+                )}
+              </div>
+            );
+          }
+        }}
+      </MediaQuery>
     </div>
-    }
-    }}
-    </MediaQuery>
-    </div>
-  )
+  );
 }
 
 export default WeatherChart;
